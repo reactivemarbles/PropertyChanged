c8be95edab549aac2c60bf7a70de9c8d32433c5f
Glenn Watson <GitHub@glennwatson.net>
2021-03-13 17:20:40 +1100

    housekeeping: add creator

---
+++ src/ReactiveMarbles.PropertyChanged.SourceGenerator/SourceCreator/StringBuilderBindExtensionClassCreator.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2019-2021 ReactiveUI Association Incorporated. All rights reserved.
+// ReactiveUI Association Incorporated licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReactiveMarbles.PropertyChanged.SourceGenerator
+{
+    internal class StringBuilderBindExtensionClassCreator : ISourceCreator
+    {
+        public string Create(MultiExpressionMethodDatum methodDatum) => throw new NotImplementedException();
+
+        public string Create(SingleExpressionDictionaryImplMethodDatum methodDatum) => throw new NotImplementedException();
+
+        public string Create(SingleExpressionOptimizedImplMethodDatum methodDatum) => throw new NotImplementedException();
+    }
+}
