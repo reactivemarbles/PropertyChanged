865936eee496a046acbeba0a3c83953d9ff660e7
cabauman <cab452005@gmail.com>
2021-04-18 11:58:51 +0900

    Add tests where diagnostics are reported

---
+++ src/ReactiveMarbles.PropertyChanged.SourceGenerator.Tests/WhenChangedGeneratorTests.Diagnostics.cs
@@ -0,0 +1,235 @@
+﻿// Copyright (c) 2019-2021 ReactiveUI Association Incorporated. All rights reserved.
+// ReactiveUI Association Incorporated licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System.Linq;
+using System.Reactive.Linq;
+using Microsoft.CodeAnalysis;
+using ReactiveMarbles.PropertyChanged.SourceGenerator.Builders;
+using Xunit;
+
+namespace ReactiveMarbles.PropertyChanged.SourceGenerator.Tests
+{
+    /// <summary>
+    /// Source generator tets.
+    /// </summary>
+    public partial class WhenChangedGeneratorTests
+    {
+        private const string WhenChangedPlaceholder = "[whenchanged_invocation]";
+        private const string SourceTemplate = @"
+using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+public class HostClass : INotifyPropertyChanged
+{
+    public HostClass()
+    {
+        [whenchanged_invocation];
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    public HostClass Child { get; }
+    public string Value { get; }
+    public HostClass[] Children { get; }
+    public string[] Values { get; }
+    public Expression<Func<HostClass, string>> MyExpression => x => x.Value;
+    public Expression<Func<HostClass, string>> GetMyExpression() => x => x.Value;
+    public HostClass GetChild() => Child;
+    public string GetValue() => Value;
+}
+";
+
+        /// <summary>
+        /// Expression arguments may not be specified as a property pointing to the actual expression.
+        /// Yes: this.WhenChanged(x => x.Value).
+        /// No: this.WhenChanged(MyExpression).
+        /// </summary>
+        /// <param name="useRoslyn">A value indicating whether the Roslyn implementation should be used.</param>
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void RXM001_PropertyInvocationUsedAsAnExpressionArgument(bool useRoslyn)
+        {
+            var invocation = "this.WhenChanged(MyExpression)";
+            var source = SourceTemplate.Replace(WhenChangedPlaceholder, invocation);
+            AssertDiagnostic(source, DiagnosticWarnings.ExpressionMustBeInline, useRoslyn);
+        }
+
+        /// <summary>
+        /// Expression arguments may not be specified as a property pointing to the actual expression.
+        /// Yes: this.WhenChanged(x => x.Value).
+        /// No: this.WhenChanged(MyExpression).
+        /// </summary>
+        /// <param name="useRoslyn">A value indicating whether the Roslyn implementation should be used.</param>
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void RXM001_PropertyInvocationUsedAsAnExpressionArgument_MultiExpressionMultiChain(bool useRoslyn)
+        {
+            var invocation = "this.WhenChanged(x => x.Child.Value, MyExpression, (a, b) => $\"{a}-{b}\")";
+            var source = SourceTemplate.Replace(WhenChangedPlaceholder, invocation);
+            AssertDiagnostic(source, DiagnosticWarnings.ExpressionMustBeInline, useRoslyn);
+        }
+
+        /// <summary>
+        /// Expression arguments may not be specified as a method pointing to the actual expression.
+        /// Yes: this.WhenChanged(x => x.Value).
+        /// No: this.WhenChanged(GetMyExpression()).
+        /// </summary>
+        /// <param name="useRoslyn">A value indicating whether the Roslyn implementation should be used.</param>
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void RXM001_MethodInvocationUsedAsAnExpressionArgument(bool useRoslyn)
+        {
+            var invocation = "this.WhenChanged(GetMyExpression())";
+            var source = SourceTemplate.Replace(WhenChangedPlaceholder, invocation);
+            AssertDiagnostic(source, DiagnosticWarnings.ExpressionMustBeInline, useRoslyn);
+        }
+
+        /// <summary>
+        /// Expression arguments may not be specified as a method pointing to the actual expression.
+        /// Yes: this.WhenChanged(x => x.Value).
+        /// No: this.WhenChanged(GetMyExpression()).
+        /// </summary>
+        /// <param name="useRoslyn">A value indicating whether the Roslyn implementation should be used.</param>
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void RXM001_MethodInvocationUsedAsAnExpressionArgument_MultiExpressionMultiChain(bool useRoslyn)
+        {
+            var invocation = "this.WhenChanged(x => x.Child.Value, GetMyExpression(), (a, b) => $\"{a}-{b}\")";
+            var source = SourceTemplate.Replace(WhenChangedPlaceholder, invocation);
+            AssertDiagnostic(source, DiagnosticWarnings.ExpressionMustBeInline, useRoslyn);
+        }
+
+        /// <summary>
+        /// Expressions may not include method invocation anywhere in the chain.
+        /// Yes: this.WhenChanged(x => x.Child.Value).
+        /// No: this.WhenChanged(x => x.GetChild().Value).
+        /// No: this.WhenChanged(x => x.Child.GetValue()).
+        /// </summary>
+        /// <param name="useRoslyn">A value indicating whether the Roslyn implementation should be used.</param>
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void RXM002_MethodInvocationUsedInExpressionChain(bool useRoslyn)
+        {
+            var invocation = "this.WhenChanged(x => x.GetValue())";
+            var source = SourceTemplate.Replace(WhenChangedPlaceholder, invocation);
+            AssertDiagnostic(source, DiagnosticWarnings.OnlyPropertyAndFieldAccessAllowed, useRoslyn);
+        }
+
+        /// <summary>
+        /// Expressions may not include method invocation anywhere in the chain.
+        /// Yes: this.WhenChanged(x => x.Child.Value).
+        /// No: this.WhenChanged(x => x.GetChild().Value).
+        /// No: this.WhenChanged(x => x.Child.GetValue()).
+        /// </summary>
+        /// <param name="useRoslyn">A value indicating whether the Roslyn implementation should be used.</param>
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void RXM002_MethodInvocationUsedInExpressionChain_MultiExpressionMultiChain(bool useRoslyn)
+        {
+            var invocation = "this.WhenChanged(x => x.Value, x => x.GetChild().Value, (a, b) => $\"{a}-{b}\")";
+            var source = SourceTemplate.Replace(WhenChangedPlaceholder, invocation);
+            AssertDiagnostic(source, DiagnosticWarnings.OnlyPropertyAndFieldAccessAllowed, useRoslyn);
+        }
+
+        /// <summary>
+        /// Expressions may not include indexer access anywhere in the chain.
+        /// Yes: this.WhenChanged(x => x.Child.Value).
+        /// No: this.WhenChanged(x => x.GetChild[0].Value).
+        /// No: this.WhenChanged(x => x.Child.GetValue["key"]).
+        /// </summary>
+        /// <param name="useRoslyn">A value indicating whether the Roslyn implementation should be used.</param>
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void RXM002_IndexerAccessUsedInExpressionChain(bool useRoslyn)
+        {
+            var invocation = "this.WhenChanged(x => x.Values[0])";
+            var source = SourceTemplate.Replace(WhenChangedPlaceholder, invocation);
+            AssertDiagnostic(source, DiagnosticWarnings.OnlyPropertyAndFieldAccessAllowed, useRoslyn);
+        }
+
+        /// <summary>
+        /// Expressions may not include indexer access anywhere in the chain.
+        /// Yes: this.WhenChanged(x => x.Child.Value).
+        /// No: this.WhenChanged(x => x.GetChild[0].Value).
+        /// No: this.WhenChanged(x => x.Child.GetValue["key"]).
+        /// </summary>
+        /// <param name="useRoslyn">A value indicating whether the Roslyn implementation should be used.</param>
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void RXM002_IndexerAccessUsedInExpressionChain_MultiExpressionMultiChain(bool useRoslyn)
+        {
+            var invocation = "this.WhenChanged(x => x.Value, x => x.Children[0].Value, (a, b) => $\"{a}-{b}\")";
+            var source = SourceTemplate.Replace(WhenChangedPlaceholder, invocation);
+            AssertDiagnostic(source, DiagnosticWarnings.OnlyPropertyAndFieldAccessAllowed, useRoslyn);
+        }
+
+        /// <summary>
+        /// Expressions may not exclude the lambda parameter.
+        /// Yes: this.WhenChanged(x => x.Child.Value).
+        /// No: this.WhenChanged(x => Child.Value).
+        /// </summary>
+        /// <param name="useRoslyn">A value indicating whether the Roslyn implementation should be used.</param>
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void RXM003_LambdaParameterNotUsed(bool useRoslyn)
+        {
+            var invocation = "this.WhenChanged(x => Value)";
+            var source = SourceTemplate.Replace(WhenChangedPlaceholder, invocation);
+            AssertDiagnostic(source, DiagnosticWarnings.LambdaParameterMustBeUsed, useRoslyn);
+        }
+
+        /// <summary>
+        /// Expressions may not exclude the lambda parameter.
+        /// Yes: this.WhenChanged(x => x.Child.Value).
+        /// No: this.WhenChanged(x => Child.Value).
+        /// </summary>
+        /// <param name="useRoslyn">A value indicating whether the Roslyn implementation should be used.</param>
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void RXM003_LambdaParameterNotUsed_MultiExpressionMultiChain(bool useRoslyn)
+        {
+            var invocation = "this.WhenChanged(x => x.Value, x => Child.Value, (a, b) => $\"{a}-{b}\")";
+            var source = SourceTemplate.Replace(WhenChangedPlaceholder, invocation);
+            AssertDiagnostic(source, DiagnosticWarnings.LambdaParameterMustBeUsed, useRoslyn);
+        }
+
+        private static void AssertDiagnostic(string source, DiagnosticDescriptor expectedDiagnostic, bool useRoslyn)
+        {
+            Compilation compilation = CompilationUtil.CreateCompilation(source);
+            var newCompilation = CompilationUtil.RunGenerators(compilation, out var generatorDiagnostics, new Generator(useRoslyn));
+            var compilationDiagnostics = newCompilation.GetDiagnostics();
+            var compilationErrors = compilationDiagnostics.Where(x => x.Severity == DiagnosticSeverity.Error).Select(x => x.GetMessage()).ToList();
+
+            Assert.Empty(compilationErrors);
+            Assert.Single(generatorDiagnostics);
+            Assert.Equal(expectedDiagnostic, generatorDiagnostics[0].Descriptor);
+        }
+
+        internal static class Method
+        {
+            public const string GetChild = "GetChild()";
+            public const string GetValue = "GetValue()";
+            public const string GetMyExpression = "GetMyExpression()";
+        }
+
+        internal static class Property
+        {
+            public const string Child = "Child";
+            public const string Value = "Value";
+            public const string Children = "Children";
+            public const string Values = "Values";
+            public const string MyExpression = "MyExpression";
+        }
+    }
+}
---
+++ src/ReactiveMarbles.PropertyChanged.SourceGenerator.Tests/WhenChangedGeneratorTests.NoDiagnostics.cs
@@ -0,0 +1,273 @@
+﻿// Copyright (c) 2019-2021 ReactiveUI Association Incorporated. All rights reserved.
+// ReactiveUI Association Incorporated licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using Microsoft.CodeAnalysis;
+using ReactiveMarbles.PropertyChanged.SourceGenerator.Builders;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ReactiveMarbles.PropertyChanged.SourceGenerator.Tests
+{
+    /// <summary>
+    /// Source generator tets.
+    /// </summary>
+    public partial class WhenChangedGeneratorTests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WhenChangedGeneratorTests"/> class.
+        /// </summary>
+        /// <param name="testOutputHelper">The output provided by xUnit.</param>
+        public WhenChangedGeneratorTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        /// <summary>
+        /// Make sure namespaces are handled correctly.
+        /// </summary>
+        /// <param name="hostTypeNamespace">The namespace name containing the host.</param>
+        /// <param name="hostPropertyTypeNamespace">The namespace name containing the property type.</param>
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData("HostNamespace", "CustomNamespace")]
+        [InlineData("HostNamespace", null)]
+        [InlineData(null, "CustomNamespace")]
+        public void NoDiagnostics_Namespaces(string hostTypeNamespace, string hostPropertyTypeNamespace)
+        {
+            var hostPropertyTypeInfo = new EmptyClassBuilder()
+                .WithClassAccess(Accessibility.Public)
+                .WithNamespace(hostPropertyTypeNamespace);
+            var hostTypeInfo = new WhenChangedHostBuilder()
+                .WithClassName("Host")
+                .WithInvocation(InvocationKind.MemberAccess, ReceiverKind.This, x => x.Child, x => x.Value, (a, b) => "result" + a + b)
+                .WithClassAccess(Accessibility.Public)
+                .WithPropertyType(hostPropertyTypeInfo)
+                .WithPropertyAccess(Accessibility.Public)
+                .WithNamespace(hostTypeNamespace);
+
+            var fixture = WhenChangedFixture.Create(hostTypeInfo, _testOutputHelper, hostPropertyTypeInfo.BuildRoot());
+            fixture.RunGenerator(out var compilationDiagnostics, out var generatorDiagnostics, useRoslyn: true, saveCompilation: false);
+
+            Assert.Empty(generatorDiagnostics.Where(x => x.Severity >= DiagnosticSeverity.Warning));
+            Assert.Empty(compilationDiagnostics.Where(x => x.Severity >= DiagnosticSeverity.Warning));
+
+            string Convert(WhenChangedHostProxy host)
+            {
+                return "result" + host.Child + host.Value;
+            }
+
+            var host = fixture.NewHostInstance();
+            host.Value = fixture.NewValuePropertyInstance();
+            var observable = host.GetWhenChangedObservable(_ => _testOutputHelper.WriteLine(fixture.Sources));
+            object value = null;
+            observable.Subscribe(x => value = x);
+            Assert.Equal(Convert(host), value);
+            host.Value = fixture.NewValuePropertyInstance();
+            Assert.Equal(Convert(host), value);
+            host.Value = null;
+            Assert.Equal(Convert(host), value);
+        }
+
+        /// <summary>
+        /// Make sure types are fully qualified by namespace in the case that there are
+        /// multiple classes with the same name.
+        /// </summary>
+        [Fact]
+        public void NoDiagnostics_SameClassName()
+        {
+            var hostPropertyTypeInfo = new EmptyClassBuilder()
+                .WithClassName("ClassName")
+                .WithNamespace("Namespace1")
+                .WithClassAccess(Accessibility.Public);
+            var hostTypeInfo = new WhenChangedHostBuilder()
+                .WithClassName("ClassName")
+                .WithNamespace("Namespace2")
+                .WithInvocation(InvocationKind.MemberAccess, ReceiverKind.This, x => x.Child, x => x.Value, (a, b) => "result" + a + b)
+                .WithClassAccess(Accessibility.Public)
+                .WithPropertyType(hostPropertyTypeInfo)
+                .WithPropertyAccess(Accessibility.Public);
+
+            var fixture = WhenChangedFixture.Create(hostTypeInfo, _testOutputHelper, hostPropertyTypeInfo.BuildRoot());
+            fixture.RunGenerator(out var compilationDiagnostics, out var generatorDiagnostics, useRoslyn: true, saveCompilation: false);
+
+            Assert.Empty(generatorDiagnostics.Where(x => x.Severity >= DiagnosticSeverity.Warning));
+            Assert.Empty(compilationDiagnostics.Where(x => x.Severity >= DiagnosticSeverity.Warning));
+
+            string Convert(WhenChangedHostProxy host)
+            {
+                return "result" + host.Child + host.Value;
+            }
+
+            var host = fixture.NewHostInstance();
+            host.Value = fixture.NewValuePropertyInstance();
+            var observable = host.GetWhenChangedObservable(_ => _testOutputHelper.WriteLine(fixture.Sources));
+            object value = null;
+            observable.Subscribe(x => value = x);
+            Assert.Equal(Convert(host), value);
+            host.Value = fixture.NewValuePropertyInstance();
+            Assert.Equal(Convert(host), value);
+            host.Value = null;
+            Assert.Equal(Convert(host), value);
+        }
+
+        /// <summary>
+        /// Make sure chains of length 2 are handled correctly.
+        /// </summary>
+        [Fact]
+        public void NoDiagnostics_ChainOf2()
+        {
+            var hostPropertyTypeInfo = new EmptyClassBuilder()
+                .WithClassName("ClassName")
+                .WithNamespace("Namespace1")
+                .WithClassAccess(Accessibility.Public);
+            var hostTypeInfo = new WhenChangedHostBuilder()
+                .WithClassName("ClassName")
+                .WithNamespace("Namespace2")
+                .WithInvocation(InvocationKind.MemberAccess, ReceiverKind.This, x => x.Child.Value)
+                .WithClassAccess(Accessibility.Public)
+                .WithPropertyType(hostPropertyTypeInfo)
+                .WithPropertyAccess(Accessibility.Public);
+
+            var fixture = WhenChangedFixture.Create(hostTypeInfo, _testOutputHelper, hostPropertyTypeInfo.BuildRoot());
+            fixture.RunGenerator(out var compilationDiagnostics, out var generatorDiagnostics, useRoslyn: true, saveCompilation: false);
+
+            Assert.Empty(generatorDiagnostics.Where(x => x.Severity >= DiagnosticSeverity.Warning));
+            Assert.Empty(compilationDiagnostics.Where(x => x.Severity >= DiagnosticSeverity.Warning));
+
+            var host = fixture.NewHostInstance();
+            host.Child = fixture.NewHostInstance();
+            host.Child.Value = fixture.NewValuePropertyInstance();
+            var observable = host.GetWhenChangedObservable(_ => _testOutputHelper.WriteLine(fixture.Sources));
+            object value = null;
+            observable.Subscribe(x => value = x);
+            Assert.Equal(host.Child.Value, value);
+
+            // According to the current design, no emission should occur when the chain is "broken".
+            host.Child = null;
+            Assert.NotNull(value);
+
+            host.Child = fixture.NewHostInstance();
+            Assert.Equal(host.Child.Value, value);
+            host.Child.Value = fixture.NewValuePropertyInstance();
+            Assert.Equal(host.Child.Value, value);
+        }
+
+        /// <summary>
+        /// Make sure chains of length 3 are handled correctly.
+        /// </summary>
+        [Fact]
+        public void NoDiagnostics_ChainOf3()
+        {
+            var hostPropertyTypeInfo = new EmptyClassBuilder()
+                .WithClassName("ClassName")
+                .WithNamespace("Namespace1")
+                .WithClassAccess(Accessibility.Public);
+            var hostTypeInfo = new WhenChangedHostBuilder()
+                .WithClassName("ClassName")
+                .WithNamespace("Namespace2")
+                .WithInvocation(InvocationKind.MemberAccess, ReceiverKind.This, x => x.Child.Child.Value)
+                .WithClassAccess(Accessibility.Public)
+                .WithPropertyType(hostPropertyTypeInfo)
+                .WithPropertyAccess(Accessibility.Public);
+
+            var fixture = WhenChangedFixture.Create(hostTypeInfo, _testOutputHelper, hostPropertyTypeInfo.BuildRoot());
+            fixture.RunGenerator(out var compilationDiagnostics, out var generatorDiagnostics, useRoslyn: true, saveCompilation: false);
+
+            Assert.Empty(generatorDiagnostics.Where(x => x.Severity >= DiagnosticSeverity.Warning));
+            Assert.Empty(compilationDiagnostics.Where(x => x.Severity >= DiagnosticSeverity.Warning));
+
+            var host = fixture.NewHostInstance();
+            host.Child = fixture.NewHostInstance();
+            host.Child.Child = fixture.NewHostInstance();
+            host.Child.Child.Value = fixture.NewValuePropertyInstance();
+            var observable = host.GetWhenChangedObservable(_ => _testOutputHelper.WriteLine(fixture.Sources));
+            object value = null;
+            observable.Subscribe(x => value = x);
+            Assert.Equal(host.Child.Child.Value, value);
+
+            // According to the current design, no emission should occur when the chain is "broken".
+            host.Child = null;
+            Assert.NotNull(value);
+
+            host.Child = fixture.NewHostInstance();
+            host.Child.Child = fixture.NewHostInstance();
+            Assert.Equal(host.Child.Value, value);
+            host.Child.Child.Value = fixture.NewValuePropertyInstance();
+            Assert.Equal(host.Child.Child.Value, value);
+        }
+
+        /// <summary>
+        /// Tests that no diagnostics are reported when property access modifier is private.
+        /// </summary>
+        /// <param name="hostContainerTypeAccess">outerClassAccess.</param>
+        /// <param name="hostTypeAccess">hostClassAccess.</param>
+        /// <param name="propertyTypeAccess">outputClassAccess.</param>
+        /// <param name="propertyAccess">propertyAccess.</param>
+        [Theory]
+        [MemberData(nameof(AccessibilityTestCases.GetValidAccessModifierCombinations), MemberType = typeof(AccessibilityTestCases))]
+        public void NoDiagnostics_AccessModifierCombinations_Roslyn(Accessibility hostContainerTypeAccess, Accessibility hostTypeAccess, Accessibility propertyTypeAccess, Accessibility propertyAccess)
+        {
+            NoDiagnostics_AccessModifierCombinations(hostContainerTypeAccess, hostTypeAccess, propertyTypeAccess, propertyAccess, true);
+        }
+
+        /// <summary>
+        /// Tests that no diagnostics are reported when property access modifier is private.
+        /// </summary>
+        /// <param name="hostContainerTypeAccess">outerClassAccess.</param>
+        /// <param name="hostTypeAccess">hostClassAccess.</param>
+        /// <param name="propertyTypeAccess">outputClassAccess.</param>
+        /// <param name="propertyAccess">propertyAccess.</param>
+        [Theory]
+        [MemberData(nameof(AccessibilityTestCases.GetValidAccessModifierCombinations), MemberType = typeof(AccessibilityTestCases))]
+        public void NoDiagnostics_AccessModifierCombinations_StringBuilder(Accessibility hostContainerTypeAccess, Accessibility hostTypeAccess, Accessibility propertyTypeAccess, Accessibility propertyAccess)
+        {
+            NoDiagnostics_AccessModifierCombinations(hostContainerTypeAccess, hostTypeAccess, propertyTypeAccess, propertyAccess, false);
+        }
+
+        private void NoDiagnostics_AccessModifierCombinations(Accessibility hostContainerTypeAccess, Accessibility hostTypeAccess, Accessibility propertyTypeAccess, Accessibility propertyAccess, bool useRoslyn)
+        {
+            var hostPropertyTypeInfo = new EmptyClassBuilder()
+                .WithClassAccess(propertyTypeAccess);
+            var hostTypeInfo = new WhenChangedHostBuilder()
+                .WithClassName("Host")
+                .WithInvocation(InvocationKind.MemberAccess, ReceiverKind.This, x => x.Child, x => x.Value, (a, b) => "result" + a + b)
+                .WithClassAccess(hostTypeAccess)
+                .AddNestedClass(hostPropertyTypeInfo)
+                .WithPropertyType(hostPropertyTypeInfo)
+                .WithPropertyAccess(propertyAccess);
+            var hostContainerTypeInfo = new EmptyClassBuilder()
+                .WithClassName("HostContainer")
+                .WithClassAccess(hostContainerTypeAccess)
+                .AddNestedClass(hostTypeInfo);
+
+            var fixture = WhenChangedFixture.Create(hostTypeInfo, _testOutputHelper);
+            fixture.RunGenerator(out var compilationDiagnostics, out var generatorDiagnostics, useRoslyn, saveCompilation: false);
+
+            Assert.Empty(generatorDiagnostics.Where(x => x.Severity >= DiagnosticSeverity.Warning));
+            Assert.Empty(compilationDiagnostics.Where(x => x.Severity >= DiagnosticSeverity.Warning));
+
+            string Convert(WhenChangedHostProxy host)
+            {
+                return "result" + host.Child + host.Value;
+            }
+
+            var host = fixture.NewHostInstance();
+            host.Value = fixture.NewValuePropertyInstance();
+            var observable = host.GetWhenChangedObservable(_ => _testOutputHelper.WriteLine(fixture.Sources));
+            object value = null;
+            observable.Subscribe(x => value = x);
+            Assert.Equal(Convert(host), value);
+            host.Value = fixture.NewValuePropertyInstance();
+            Assert.Equal(Convert(host), value);
+            host.Value = null;
+            Assert.Equal(Convert(host), value);
+        }
+    }
+}
