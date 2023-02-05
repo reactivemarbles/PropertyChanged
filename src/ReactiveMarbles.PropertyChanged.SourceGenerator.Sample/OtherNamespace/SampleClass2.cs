5bfe8b94e9d54733b4925ac39ca563c6a19e887a
Chris Pulman <chris.pulman@yahoo.com>
2023-02-05 00:34:39 +0000

    Update Date, Fix T4 code to match current source in T4 templates

--- src/ReactiveMarbles.PropertyChanged.SourceGenerator.Sample/OtherNamespace/SampleClass2.cs
+++ src/ReactiveMarbles.PropertyChanged.SourceGenerator.Sample/OtherNamespace/SampleClass2.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2019-2021 ReactiveUI Association Incorporated. All rights reserved.
+﻿// Copyright (c) 2019-2023 ReactiveUI Association Incorporated. All rights reserved.
 // ReactiveUI Association Incorporated licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
@@ -11,46 +11,47 @@ namespace ReactiveMarbles.PropertyChanged.SourceGenerator.Sample.OtherNamespace
     /// </summary>
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Readability", "RCS1018:Add accessibility modifiers.", Justification = "Because")]
     [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1400:Access modifier should be declared", Justification = "Because")]
-    public class SampleClass : INotifyPropertyChanged, INotifyPropertyChanging
+    public class SampleClass2 : INotifyPropertyChanged, INotifyPropertyChanging
     {
-        private Sample.SampleClass _myClass;
-        private string _myString;
-        private string _myString2;
-        private string _myString3;
+        ////private SampleClass3 _myClass = new SampleClass3();
+        private string? _myString = string.Empty;
+        private string? _myString2 = string.Empty;
+        private string? _myString3 = string.Empty;
 
-        internal SampleClass()
+        internal SampleClass2()
         {
+           // this.WhenChanged(x => x.MyClass);
         }
 
         /// <summary>
         /// Dummy.
         /// </summary>
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
 
         /// <summary>
         /// Dummy.
         /// </summary>
-        public event PropertyChangingEventHandler PropertyChanging;
+        public event PropertyChangingEventHandler? PropertyChanging;
 
         /// <summary>
         /// Gets or sets a string.
         /// </summary>
-        internal string MyString
+        internal string? MyString1
         {
             get => _myString;
 
             set
             {
-                PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(nameof(MyString)));
+                PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(nameof(MyString1)));
                 _myString = value;
-                PropertyChanged?.Invoke(this, new(nameof(MyString)));
+                PropertyChanged?.Invoke(this, new(nameof(MyString1)));
             }
         }
 
         /// <summary>
         /// Gets or sets a string.
         /// </summary>
-        internal string MyString2
+        internal string? MyString2
         {
             get => _myString2;
 
@@ -64,7 +65,7 @@ namespace ReactiveMarbles.PropertyChanged.SourceGenerator.Sample.OtherNamespace
         /// <summary>
         /// Gets or sets a string.
         /// </summary>
-        internal string MyString3
+        internal string? MyString3
         {
             get => _myString3;
 
@@ -75,19 +76,20 @@ namespace ReactiveMarbles.PropertyChanged.SourceGenerator.Sample.OtherNamespace
             }
         }
 
-        /// <summary>
-        /// Gets or sets a class.
-        /// </summary>
-        internal Sample.SampleClass MyClass
-        {
-            get => _myClass;
+        // TODO: Fix this.
+        /////// <summary>
+        /////// Gets or sets a class.
+        /////// </summary>
+        ////internal SampleClass3 MyClass
+        ////{
+        ////    get => _myClass;
 
-            set
-            {
-                PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(nameof(MyClass)));
-                _myClass = value;
-                PropertyChanged?.Invoke(this, new(nameof(MyClass)));
-            }
-        }
+        ////    set
+        ////    {
+        ////        PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(nameof(MyClass)));
+        ////        _myClass = value;
+        ////        PropertyChanged?.Invoke(this, new(nameof(MyClass)));
+        ////    }
+        ////}
     }
 }
