cf1d1d9eb05882027a7310870dabf89b6ee5be91
Chris Pulman <chris.pulman@yahoo.com>
2025-07-13 17:28:50 +0100

    Update StyleCop.Analyzers and minor code formatting
    Bump StyleCop.Analyzers to version 1.2.0-beta.556 in Directory.Build.props. Add a blank line for formatting in MemberFuncCacheKeyComparer.cs.

--- src/ReactiveMarbles.PropertyChanged/MemberFuncCacheKeyComparer.cs
+++ src/ReactiveMarbles.PropertyChanged/MemberFuncCacheKeyComparer.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Reflection;
 
 namespace ReactiveMarbles.PropertyChanged;
+
 #if !UIKIT
 internal sealed class MemberFuncCacheKeyComparer : IEqualityComparer<MemberInfo>
 {
